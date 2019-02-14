f1e3bc1d45e6b3ecdca0de7b12cef030e1a5ce8c
Frank Xia <FXia@esri.com>
2019-02-14 11:21:18 -0500

    added code for auto-selecting solr times from text outputs

--- src/main/java/com/esri/arcgis/dse/test/Utils.java
+++ src/main/java/com/esri/arcgis/dse/test/Utils.java
@@ -1,12 +1,50 @@
 package com.esri.arcgis.dse.test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Utils {
 
   public static void main(String[] args) {
-    if (args.length <= 1) {
-      System.out.println("Usage: java -cp ./target/ms-solr-api-performance-1.0.jar com.esri.arcgis.dse.test.Utils <List of numbers> ");
+    if (args.length == 0) {
+      System.out.println("Usage: java -cp ./target/ms-solr-api-performance-1.0.jar com.esri.arcgis.dse.test.Utils <List of numbers> Or <File name>");
+    } else if (args.length == 1) {
+      computeStats(args[0]);
     } else {
       computeStats(args);
+    }
+  }
+
+  private static void computeStats(String fileName) {
+    File file = new File(fileName);
+    String prefix = "Solr request time: ";
+    try {
+      if (file.exists()) {
+        List<String> data = new LinkedList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = reader.readLine();
+        while (line != null) {
+          if (line.startsWith(prefix)) {
+            line = line.substring(prefix.length()).trim();
+            String[] splits = line.split(" ");
+            if (splits.length == 2) {
+              data.add(splits[0]);
+              System.out.println(splits[0]);
+            }
+          }
+          line = reader.readLine();
+        }
+        reader.close();
+
+        computeStats(data.toArray(new String[0]));
+      } else {
+        System.out.println("File '" + fileName + "' does not exist!");
+      }
+    }catch (Exception ex) {
+      ex.printStackTrace();
     }
   }
 
@@ -27,6 +65,7 @@ public class Utils {
     double avg = sum / data.length;
     double std_dev = Math.sqrt( (squaredValue - data.length * avg * avg) / (data.length - 1) );
 
+    System.out.println("Total data points: " + data.length);
     System.out.println("Average, min, max and std_dev: " + avg +  " " + min + " " +max + " " + std_dev);
   }
 }
